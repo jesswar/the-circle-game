99ed99a6f390c9f4c1e650470cebd798e4f013ab
jesswar <jessicalsmyers@gmail.com>
2015-04-17 12:54:28 -0700

    javascript logic

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log( 'I am javascript.' );
+
+
+
+// when circle is clicked, attach circle to cursor.
+// when cursor moves, circle follows underneath.
+// when cursor clicks outside of holder, return circle to original spot.
+// when cursor clicks inside holder, snap circle to holder.
+// when circle snaps to holder, fade out elements h1, h2, and circles, and fade in new stuff.
+
+
+
+
+var myCircle = document.getElementById(‘circle’);
+
+ function moveit(dmove, gtop, gleft, numvalid){
+	dmove.style.top = gtop.value + 'px';
+	dmove.style.left = gleft.value + 'px';
+ }
